2ae8e749e0b6e5d6950989d8d6a6180c5fd9b1cf
Mr Wolf <abit@itu.dk>
2016-09-05 12:51:34 +0200

    Fixes the input parsing, seems there is a bug infinite loop

--- matching/solution/StableMatching.java
+++ matching/solution/StableMatching.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 
 public class StableMatching{
 	static Map<Integer, Partner> tinder;
-	Map<Integer, String> map;
+	private Map<Integer, String> map;
 	private Scanner input;
 	private int couples;
+
 	public StableMatching(Scanner input){
 		this.input = input;
 	}
@@ -20,8 +21,8 @@ public class StableMatching{
 		HashMap<Integer, String> tmp = new HashMap<>();
 		input.nextLine();input.nextLine();
 		couples = ( Integer.parseInt(input.nextLine().substring(2,3)) * 2);
-		System.out.println(couples);
-		for(int i = 1; i <= couples; ++i){
+		Groom.N = couples / 2;
+		for(int i = 1; i <= couples; i++){
 			input.next();
 			tmp.put(i, input.next());
 		}
@@ -29,29 +30,57 @@ public class StableMatching{
 		map = tmp;
 	}
 	
-	private String[] extractArray(String choices){
-		//Fix the choice
-		return null;
+	private int[] extractArray(String choices){
+		String[] str = choices.split(":");
+		choices = str[1].trim(); 
+		String[] tmp = choices.split(" ");
+		int[] arr = new int[tmp.length];
+		int count = 0;
+		for(String str1 : tmp)
+			arr[count++] = Integer.parseInt(str1);
+		return arr; 
 	}
 	
 	private void createTinder(){
 		String[] grooms, juliets;
-		for(int i = 1; i <= couples; ++i){
+		String choices ;
+		Bride bride; Groom groom;
+		tinder = new HashMap<>();
+		for(int i = 1; i <= couples; i++){
 			if(i % 2 == 0){ //Brides
-					
+				bride = new Bride(i, extractArray(input.nextLine()));	
+				tinder.put(i, bride);
 			}else { //Grooms
-				
+				groom = new Groom(i, extractArray(input.nextLine()));
+				tinder.put(i, groom);	
+				Groom.singleMales.add(i);
 			}
 		}
 	}
-	 
+
 	public static void main(String args[]){
 		try(Scanner input = new Scanner(System.in)){
 			StableMatching stable = new StableMatching(input);
 			stable.createMap();
 			stable.createTinder();	
-			
+			while(true){
+				if(Groom.N == 0){
+					printMatchings();
+					return;	
+				}else{
+					Groom groom =  null;
+					for(Integer id : Groom.singleMales){
+							groom = (Groom) tinder.get(id);
+							break;
+					}
+					while(!groom.isEngaged()){
+						groom.propose();
+					}	
+				}
+			}
 		}
+	}
+	private static void printMatchings(){
 	}
 	
 }
@@ -67,9 +96,10 @@ class Partner{
 
 class Groom extends Partner{
 	//Change this with another more efficient DS
-	static ArrayList<Groom>  singleMales = new ArrayList<>(); 
+	static ArrayList<Integer>  singleMales = new ArrayList<>(); 
 	private final int[] juliets;
 	private int counter;
+	static int N;
 	private boolean isEngaged;
 
 	public Groom(int id, int[] juliets){
@@ -86,11 +116,16 @@ class Groom extends Partner{
 	
 	public void youAreNotSingleAnymore(){
 		this.isEngaged = true;
-	singleMales.remove(this);
+		singleMales.remove(this.id);
+		N--;
 	}
 	public void itIsNotYouItIsMe(){
 		this.isEngaged = false;
-		singleMales.add(this);
+		singleMales.add(this.id);
+		N++;
+	}
+	public boolean isEngaged(){
+		return isEngaged;
 	}
 	
 }
@@ -112,11 +147,10 @@ class Bride extends Partner{
 		if(currentGroom == null){
 			wannabeGroom.youAreNotSingleAnymore();
 			currentGroom = wannabeGroom;
-			return ;
 		}else{
 			int currentGroomId = currentGroom.getId();
 			int wannabeGroomId = wannabeGroom.getId();
-			if(doINeedSomeSpace.get(wannabeGroomId).compareTo(doINeedSomeSpace.get(currentGroomId)) > 0){
+			if(doINeedSomeSpace.get(wannabeGroomId).compareTo(doINeedSomeSpace.get(currentGroomId)) < 0){
 				currentGroom.itIsNotYouItIsMe();
 				wannabeGroom.youAreNotSingleAnymore();
 				currentGroom = wannabeGroom;
