5732dec50257c11950e967bf972a83fd993fc706
anb <anb@carecom.dk>
2016-09-15 19:42:48 +0200

    Week2 half

--- closest-points/solution/ClosestPoints.java
+++ closest-points/solution/ClosestPoints.java
@@ -5,15 +5,15 @@ import java.util.Arrays;
 
 public class ClosestPoints{
     private Point[] points;
-    private static Comparator<ClosestPoints.Point> xComparator = new Comparator<ClosestPoints.Point>(){
-		public int compare(ClosestPoints.Point p1, ClosestPoints.Point p2){
+    private static Comparator<Point> xComparator = new Comparator<Point>(){
+		public int compare(Point p1, Point p2){
 		    Double x1 = p1.getX(),
 				   x2 = p2.getX();
 		    return x1.compareTo(x2); 
 		}
     };
-    private static Comparator<ClosestPoints.Point> yComparator = new Comparator<ClosestPoints.Point>(){
-		public int compare(ClosestPoints.Point p1, ClosestPoints.Point p2){
+    private static Comparator<Point> yComparator = new Comparator<Point>(){
+		public int compare(Point p1, Point p2){
 		    Double y1 = p1.getY(),
 				   y2 = p2.getY();
 				return y1.compareTo(y2); 
@@ -31,9 +31,14 @@ public class ClosestPoints{
 				new Point(input.nextInt(),input.nextDouble(), input.nextDouble());
 			list.add(p);
 		}
+		int count = 0;
+		Point[] points = new Point[list.size()];
+		for(Point p : list)
+		    points[count++] = p;
 		ClosestPoints cp = 
-				new ClosestPoints((ClosestPoints.Point[]) list.toArray());
+				new ClosestPoints(points);
 		cp.closestPair();
+		
 	}
 
     public ClosestPoints(Point[] points){
@@ -55,30 +60,30 @@ public class ClosestPoints{
 	    return null;
 	}
 		
-	private static class Point{
-		final double xCoordinate;
-		final double yCoordinate;
-		final int id;
+}
+class Point{
+    final double xCoordinate;
+	final double yCoordinate;
+	final int id;
 
-		Point(int id, double x, double y ){
-			xCoordinate = x;
-			yCoordinate = y;
-			this.id = id;
-		}
-		double d(Point p){
-			return Math.sqrt(Math.pow((this.getX() - p.getX()), 2) + Math.pow((this.getY() - p.getY()), 2));
-		}
-		public double getX(){
-			return this.xCoordinate;
-		}
-		public double getY(){
-			return this.yCoordinate;
-		}
-		public String toString(){
-				String str =   "id: "+this.id + " x: " + this.getX() + " y: " + this.getY();
-				return str;
-		}
-
-		
+	Point(int id, double x, double y ){
+		xCoordinate = x;
+		yCoordinate = y;
+		this.id = id;
 	}
+	double d(Point p){
+		if(p == this)
+		    return Double.MAX_VALUE;
+		return Math.sqrt(Math.pow((this.getX() - p.getX()), 2) + Math.pow((this.getY() - p.getY()), 2));
+	}
+	public double getX(){
+		return this.xCoordinate;
+	}
+	public double getY(){
+		return this.yCoordinate;
+	}
+	public String toString(){
+			String str =   "id: "+this.id + " x: " + this.getX() + " y: " + this.getY();
+			return str;
+	}		
 }
