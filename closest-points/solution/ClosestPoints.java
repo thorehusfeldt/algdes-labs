81efd5546db2ef4e57b7538f354d002fed05b3fe
Mr Wolf <abit@itu.dk>
2016-09-16 13:54:35 +0200

    Throws NPE

--- closest-points/solution/ClosestPoints.java
+++ closest-points/solution/ClosestPoints.java
@@ -4,24 +4,24 @@ import java.util.Comparator;
 import java.util.Arrays;
 
 public class ClosestPoints{
-    private Point[] points;
-    private static Comparator<Point> xComparator = new Comparator<Point>(){
-		public int compare(Point p1, Point p2){
-		    Double x1 = p1.getX(),
-				   x2 = p2.getX();
-		    return x1.compareTo(x2); 
-		}
-    };
-    private static Comparator<Point> yComparator = new Comparator<Point>(){
-		public int compare(Point p1, Point p2){
-		    Double y1 = p1.getY(),
-				   y2 = p2.getY();
-				return y1.compareTo(y2); 
-		    }
-    };
-
+    private static Comparator<Point> xComparator = 
+			(p1, p2) -> new Double(p1.getX()).compareTo(new Double(p2.getX()));
+    private static Comparator<Point> yComparator = 
+			(p1, p2) -> new Double(p1.getY()).compareTo(new Double(p2.getY()));
+	private Scanner input;
+	Point[] points, pointsX, pointsY;
+	
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+		ClosestPoints cp = new ClosestPoints(input);
+		try{ cp.build();
+		}catch(InterruptedException e){System.exit(42);}
+
+	}
+	public ClosestPoints(Scanner input){
+		this.input = input;
+	}
+	public void build() throws InterruptedException{
 		while(!input.hasNextInt()){
 			System.out.println(input.nextLine());
 		}
@@ -32,58 +32,136 @@ public class ClosestPoints{
 			list.add(p);
 		}
 		int count = 0;
-		Point[] points = new Point[list.size()];
+		 points = new Point[list.size()];
 		for(Point p : list)
 		    points[count++] = p;
-		ClosestPoints cp = 
-				new ClosestPoints(points);
-		cp.closestPair();
+		sort();
+	}
+	private void  sort() throws InterruptedException {
+		Thread t1 = new Thread(() -> sortByX());
+		Thread t2 = new Thread(() -> sortByY());
+		t1.start(); t2.start();
+		t1.join();t2.join();
+		closestPair();
+	}
+
+    private void sortByX(){
+		int count = 0;
+		pointsX = new Point[points.length];
+		for(Point p: this.points)
+			pointsX[count++] = p;
+		Arrays.sort(pointsX, xComparator);
+	}
+    private void sortByY(){
+		int count = 0;
+		pointsY = new Point[points.length];
+		for(Point p : this.points)
+			pointsY[count++] = p;
+		Arrays.sort(pointsY, yComparator);
+	}
+    private Point[] closestPair(){
+		return	ClosestPairRec(pointsX, pointsY ,0, points.length);
+	}
+	private Point[] ClosestPairRec(Point[] px, Point[] py, int from, int to){
+		System.out.println("from: "+ from + ", to: " + to );
+		if(to - from <= 3){
+			return computeMinDistance(px, py);
+		}
+		final int from1= from, to1 = Math.round(Math.round(Math.ceil(from1 + to)/2)) ;
+		final int from2 = to1, to2 = to;
+		Point[][] result = constructQR(from, to);
+		Point[] qX = result[0];
+		Point[] qY = result[1];
+		Point[] rX = result[2];
+		Point[] rY = result[3];
+		Point[] left = ClosestPairRec(qX, qY, from1, to1);
+		Point[] right = ClosestPairRec(rX, rY, from2, to2);
+		double d = Math.min(left[0].distanceTo(left[1]), right[0].distanceTo(right[1]));
+
+
+			return null;
 		
 	}
 
-    public ClosestPoints(Point[] points){
-			this.points = points;
+	private static void print(Point[] parr){
+		for( Point p : parr)
+			System.out.println(p);
 	}
+	private Point[][] constructQR(int from, int to){
+		final int from1= from, to1 = Math.round(Math.round(Math.ceil(from1 + to)/2)) ;
+		final int from2 = to1, to2 = to;
+		final Point[] qY = new Point[to1 - from1];
+		final Point[] qX = new Point[to1 - from1];
+		int count = 0;
+		//Already Sort
+		Thread t1 = new Thread(() ->{
+			int count1 = from1;
+			int  size1 = to1 - from1;
+			for(int p = 0; p < size1; p++){
+				qX[p] = pointsX[count1++];
+				qY[p] = qX[p];
+			}
+			Arrays.sort(qY, yComparator);
+		});t1.start();
+		final Point[]rY = new Point[to2 - from2],
+				rX = new Point[to2 - from2];
+		Thread t2 = new Thread(() -> {
+			int size2 = to2 - from2;
+			int count2 = from2;
+			for(int p = 0 ; p < size2; p++){
+				rX[p] = pointsY[count2++];
+				rY[p] = pointsX[p];
+			}
+			Arrays.sort(rY, yComparator);
+		});t2.start();
+		try{
+			t1.join();t2.join();
+		}catch(InterruptedException e) {
+			input.close();
+			throw new RuntimeException(e);
+		}
+		return new Point[][]{qX, qY, rX, rY}; 
+	}
+	private Point[] computeMinDistance(Point[] psX, Point[] psY){
+		Point[] res = new Point[2];
+		double min = Double.MAX_VALUE;
+		for(Point px : psX){
+			for(Point py : psY){
+				if(px.distanceTo(py) < min){
+					min = px.distanceTo(px);
+					res[0] = px; res[1] = py;
+				}
+			}
+		}
+		return res;
+	}
+private static class Point{
+    	final double xCoordinate;
+		final double yCoordinate;
+		final int id; 
 
-    private Point[] sortByX(){
-		Point[] px = this.points;
-		Arrays.sort(px, xComparator);
-		return px;
-	}
-    private Point[] sortByY(){
-		Point[] py = this.points;
-		Arrays.sort(py, yComparator);
-		return py;
-	}
-    public Point[] closestPair(){
-				
-	    return null;
-	}
-		
-}
-class Point{
-    final double xCoordinate;
-	final double yCoordinate;
-	final int id;
-
-	Point(int id, double x, double y ){
-		xCoordinate = x;
-		yCoordinate = y;
-		this.id = id;
-	}
-	double d(Point p){
-		if(p == this)
-		    return Double.MAX_VALUE;
-		return Math.sqrt(Math.pow((this.getX() - p.getX()), 2) + Math.pow((this.getY() - p.getY()), 2));
-	}
-	public double getX(){
-		return this.xCoordinate;
-	}
-	public double getY(){
-		return this.yCoordinate;
-	}
-	public String toString(){
+		Point(int id, double x, double y ){
+			xCoordinate = x;
+			yCoordinate = y;
+			this.id = id;
+		}
+		double distanceTo(Point p){
+			if(p == this)
+		    	return Double.MAX_VALUE;
+			return Math.sqrt(Math.pow((this.getX() - p.getX()), 2) + Math.pow((this.getY() - p.getY()), 2));
+		}
+		public double getX(){
+			return this.xCoordinate;
+		}
+		public double getY(){
+			return this.yCoordinate;
+		}
+		public String toString(){
 			String str =   "id: "+this.id + " x: " + this.getX() + " y: " + this.getY();
 			return str;
-	}		
+		}		
+	}
+		
 }
+
+
