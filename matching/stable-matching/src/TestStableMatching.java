e639d9a23680ccbf2f7e547bc466a97fcc014aef
Godzsák Dávid <godzsakdavid@gmail.com>
2018-09-03 12:09:32 +0200

    start implementation

--- matching/stable-matching/src/TestStableMatching.java
+++ matching/stable-matching/src/TestStableMatching.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 class TestStableMatching {
@@ -52,5 +53,32 @@ class TestStableMatching {
 }
 
 class StableMatching {
+    //key = women, value = men
+    Map<Integer, Integer> M;
+    Integer n;
+    List<String> names;
+    List<List<Integer>> preferences;
+
+    public StableMatching(Integer n, List<String> names, List<List<Integer>> preferences) {
+        this.n = n;
+        this.names = names;
+        this.preferences = preferences;
+    }
+
+    public Map<Integer, Integer> gs() {
+        while (M.size() < n) {
+            for (int i = 0; i < 2 * n; i += 2) {
+                int s = preferences.get(i).get(0);
+                preferences.get(i).remove(0);
+
+                if (!M.containsValue(s)) {
+                    M.put(s, i);
+                } else if (/*s prefers h to current partner hʹ*/) {
+                    //...
+                }
+            }
+        }
+    }
+
 
 }
