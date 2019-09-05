932196a92cf9d2c40e84d7d0a584a931c5d552fe
magc <magc@itu.dk>
2019-09-05 10:29:08 +0200

    Cleanup & interface implementation of getName()

--- matching/JOBO_solution/data/scriptExample.sh
+++ matching/JOBO_solution/data/scriptExample.sh
@@ -4,6 +4,6 @@ for FILE in *-in.txt
 do
 	echo $FILE
 	base=${FILE%-in.txt}
-    java -cp '..'GS $FILE > $base.yourname.out.txt # replace with your command!
-    diff $base.yourname.out.txt $base-out.txt
+    java -cp '../src/' Main $FILE > $base.groupi.out.txt # replace with your command!
+    diff $base.groupi.out.txt $base-out.txt
 done
