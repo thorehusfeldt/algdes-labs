f8331cbc6411d8aa2be60b880fbdd6683578dfa9
magc <magc@itu.dk>
2019-11-07 12:38:25 +0100

    More files

---
+++ red-scare/src/Parser.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+	public List<Node> doParse(String filename){
+		List<Node> nodes = new ArrayList<>();
+
+		return nodes;
+	}
+}
