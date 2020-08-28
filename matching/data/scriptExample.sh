a3d4678665964167c63ed11971784a25108abc8b
Anders Fischer-Nielsen <andersfischern@me.com>
2020-08-28 12:42:15 +0200

    Remove account-specific naming in verification files

--- matching/data/scriptExample.sh
+++ matching/data/scriptExample.sh
@@ -4,6 +4,6 @@ for FILE in data/*-in.txt
 do
 	echo $FILE
 	base=${FILE%-in.txt}
-    python3 solution.py < $FILE > $base.afin.out.txt
-    python3 unordered-diff.py $base.afin.out.txt $base-out.txt
+    python3 solution.py < $FILE > $base.our_solution.out.txt
+    python3 unordered-diff.py $base.our_solution.out.txt $base-out.txt
 done
