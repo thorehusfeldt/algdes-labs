57c5473171b14ce98181dcbabce69b225b3143a0
Anders Fischer-Nielsen <andersfischern@me.com>
2020-08-28 12:46:15 +0200

    Remove output files after verification

--- matching/data/scriptExample.sh
+++ matching/data/scriptExample.sh
@@ -6,4 +6,5 @@ do
 	base=${FILE%-in.txt}
     python3 solution.py < $FILE > $base.our_solution.out.txt
     python3 unordered-diff.py $base.our_solution.out.txt $base-out.txt
+    rm $base.our_solution.out.txt
 done
