e9fa4eb96411e12e441afdb666f87d54402abdf1
Anders Fischer-Nielsen <andersfischern@me.com>
2020-08-28 13:59:43 +0200

    Reorder script

--- matching/data/scriptExample.sh
+++ matching/data/scriptExample.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-for FILE in data/*-in.txt
+for FILE in *-in.txt
 
 do
 	echo $FILE
 	base=${FILE%-in.txt}
-    python3 solution.py < $FILE > $base.our_solution.out.txt
+    python3 ../solution.py < $FILE > $base.our_solution.out.txt
     python3 unordered-diff.py $base.our_solution.out.txt $base-out.txt
     rm $base.our_solution.out.txt
 done
